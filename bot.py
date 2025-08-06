7bd0effd9ec12185b29be2b44a2809898ce2a093
Drago-King <106234416+Drago-King@users.noreply.github.com>
2025-08-06 17:22:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,16 +42,16 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/echoflex_tv for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/echoflix_tv for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/echoflex_tv")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/echoflix_tv")
         self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @Poseidon_xd""")
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Echoflex_tv")   
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @Echoflix_tv")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -59,7 +59,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Echoflix_help</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
