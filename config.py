51bd7ae4ae3f401e897e09420eb55ca8c1b840a5
Drago-King <106234416+Drago-King@users.noreply.github.com>
2025-08-06 17:43:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,17 +23,17 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
-BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/echoflex_tv")
+BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/echoflix_tv")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
+START_PIC = os.environ.get("START_PIC", "https://envs.sh/QAS.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/QAS.jpg")
 #--------------------------------------------
 
 #--------------------------------------------
 HELP_TXT = (
     "<b><blockquote>"
-    "ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋɪɴɢ ғᴏʀ @Echoflex_TV\n\n"
+    "ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋɪɴɢ ғᴏʀ @Echoflix_TV\n\n"
     "❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n"
     "├ /start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n"
     "├ /about : ʙᴏᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ\n"
@@ -74,7 +74,7 @@ CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockqu
 <b>›› /delreq :</b> Rᴇᴍᴏᴠᴇᴅ ʟᴇғᴛᴏᴠᴇʀ ɴᴏɴ-ʀᴇǫᴜᴇsᴛ ᴜsᴇʀs
 """
 #--------------------------------------------
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "ECHO FLEX TV COPYRIGHT ISSUED") #set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "ECHO FLIX TV COPYRIGHT ISSUED") #set your Custom Caption here, Keep None for Disable Custom Caption
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False #set True if you want to prevent users from forwarding files from bot
 #--------------------------------------------
 #Set true if you want Disable your Channel Posts Share button
